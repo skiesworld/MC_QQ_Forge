0826e5e8a4b3a0aa508ab974c1d2a17c9acbd96f
17TheWord <17theword@gmail.com>
2024-03-24 11:18:13 +0800

    :sparkles: 初步适配工具包

---
+++ src/main/java/com/github/theword/CommandTool.java
@@ -0,0 +1,12 @@
+package com.github.theword;
+
+import com.mojang.brigadier.context.CommandContext;
+import net.minecraft.commands.CommandSourceStack;
+import net.minecraft.network.chat.MutableComponent;
+import net.minecraft.network.chat.contents.LiteralContents;
+
+public class CommandTool {
+    public static void sendResultComponent(CommandContext<CommandSourceStack> context, String text) {
+        context.getSource().sendSystemMessage(MutableComponent.create(new LiteralContents(text)));
+    }
+}
