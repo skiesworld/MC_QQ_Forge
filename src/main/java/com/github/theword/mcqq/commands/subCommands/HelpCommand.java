34cdcc92a98debed8f81a107b995e2e3d065144f
17TheWord <17theword@gmail.com>
2024-05-23 13:13:50 +0800

    :bug: Fix recursive call help command

--- src/main/java/com/github/theword/mcqq/commands/subCommands/HelpCommand.java
+++ src/main/java/com/github/theword/mcqq/commands/subCommands/HelpCommand.java
@@ -1,29 +1,19 @@
 package com.github.theword.mcqq.commands.subCommands;
 
+import com.github.theword.mcqq.commands.CommandManager;
 import com.github.theword.mcqq.commands.ForgeSubCommand;
-import com.github.theword.mcqq.commands.subCommands.client.ReconnectCommand;
 import com.mojang.brigadier.Command;
 import com.mojang.brigadier.context.CommandContext;
 import net.minecraft.commands.CommandSourceStack;
 
-import java.util.ArrayList;
-import java.util.List;
-
 import static com.github.theword.mcqq.utils.Tool.handleCommandReturnMessage;
 
 public class HelpCommand extends HelpCommandAbstract implements ForgeSubCommand {
-    private final List<ForgeSubCommand> subCommandList = new ArrayList<>();
-
-    public HelpCommand() {
-        subCommandList.add(new HelpCommand());
-        subCommandList.add(new ReloadCommand());
-        subCommandList.add(new ReconnectCommand());
-    }
 
     @Override
     public int onCommand(CommandContext<CommandSourceStack> context) {
         handleCommandReturnMessage.handleCommandReturnMessage(context, "-------------------");
-        for (ForgeSubCommand forgeSubCommand : subCommandList) {
+        for (ForgeSubCommand forgeSubCommand : new CommandManager().getSubCommandList()) {
             handleCommandReturnMessage.handleCommandReturnMessage(context, forgeSubCommand.getUsage() + "---" + forgeSubCommand.getDescription());
         }
         handleCommandReturnMessage.handleCommandReturnMessage(context, "-------------------");
