c358dd061902b3a5aca4e1eec6e86b64a373eee1
17TheWord <17theword@gmail.com>
2024-05-23 12:43:54 +0800

    :hammer: :sparkles: Rewrite command executor

--- src/main/java/com/github/theword/mcqq/commands/CommandManager.java
+++
@@ -1,9 +0,0 @@
-package com.github.theword.mcqq.commands;
-
-import java.util.ArrayList;
-import java.util.List;
-
-public class CommandManager {
-    public static final List<SubCommand> subCommandList = new ArrayList<>();
-
-}
