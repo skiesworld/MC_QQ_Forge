783bd08b68a1f4f6a3d07d6ca0d8b3e90e84d8aa
17TheWord <17theword@gmail.com>
2024-05-24 21:41:15 +0800

    Dev 1.20.1 (#16)
    * 🐛 Package right tool name
    
    * :sparkles: Upgrade handle tool
    
    * :hammer: :sparkles: Rewrite command executor
    
    * :hammer: :sparkles: Rewrite subcommand
    
    * :sparkles: Use new tool
    
    * :fire: Remove old handle tool
    
    * :art: Remove space
    
    * :bug: Fix recursive call help command
    
    * :arrow_up: Upgrade tool version
    
    * :bookmark: Bump version 2.7.0
    
    * :construction_worker: Upgrade CI build
    
    * :construction_worker: Build when pr and publish when push main

--- src/main/java/com/github/theword/mcqq/MCQQ.java
+++ src/main/java/com/github/theword/mcqq/MCQQ.java
@@ -1,9 +1,8 @@
 package com.github.theword.mcqq;
 
-import com.github.theword.mcqq.utils.HandleWebsocketMessageService;
 import com.github.theword.mcqq.constant.WebsocketConstantMessage;
-import com.github.theword.mcqq.utils.Config;
-import com.github.theword.mcqq.websocket.WsClient;
+import com.github.theword.mcqq.handleMessage.HandleApiService;
+import com.github.theword.mcqq.handleMessage.HandleCommandReturnMessageService;
 import net.minecraft.server.MinecraftServer;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.api.distmarker.OnlyIn;
@@ -12,25 +11,17 @@ import net.minecraftforge.event.server.ServerStartingEvent;
 import net.minecraftforge.event.server.ServerStoppingEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
-import org.slf4j.LoggerFactory;
 
-import java.net.URI;
-import java.net.URISyntaxException;
-import java.util.ArrayList;
-
-import static com.github.theword.mcqq.utils.Tool.*;
+import static com.github.theword.mcqq.utils.Tool.initTool;
+import static com.github.theword.mcqq.utils.Tool.websocketManager;
 
 @Mod(MCQQ.MOD_ID)
 public class MCQQ {
     public static final String MOD_ID = "mcqq";
-    public static final String MOD_NAME = "MC_QQ";
     public static MinecraftServer minecraftServer;
 
     public MCQQ() {
-        logger = LoggerFactory.getLogger(MOD_NAME);
-        config = new Config(true);
-        wsClientList = new ArrayList<>();
-        handleWebsocketMessage = new HandleWebsocketMessageService();
+        initTool(true, new HandleApiService(), new HandleCommandReturnMessageService());
         MinecraftForge.EVENT_BUS.register(this);
         MinecraftForge.EVENT_BUS.register(new EventProcessor());
     }
@@ -38,27 +29,13 @@ public class MCQQ {
     @OnlyIn(Dist.DEDICATED_SERVER)
     @SubscribeEvent
     public void onServerStarting(ServerStartingEvent event) {
-        logger.info(WebsocketConstantMessage.WEBSOCKET_RUNNING);
         minecraftServer = event.getServer();
-        config.getWebsocketUrlList().forEach(websocketUrl -> {
-            try {
-                WsClient wsClient = new WsClient(new URI(websocketUrl));
-                wsClient.connect();
-                wsClientList.add(wsClient);
-            } catch (URISyntaxException e) {
-                logger.warn(WebsocketConstantMessage.WEBSOCKET_ERROR_URI_SYNTAX_ERROR.formatted(websocketUrl));
-            }
-        });
+        websocketManager.startWebsocket(null);
     }
 
     @OnlyIn(Dist.DEDICATED_SERVER)
     @SubscribeEvent
     public void onServerStopping(ServerStoppingEvent event) {
-        wsClientList.forEach(
-                wsClient -> wsClient.stopWithoutReconnect(
-                        1000,
-                        WebsocketConstantMessage.WEBSOCKET_CLOSING.formatted(wsClient.getURI())
-                )
-        );
+        websocketManager.stopWebsocket(1000, WebsocketConstantMessage.Client.CLOSING, null);
     }
 }
---
+++ src/main/java/com/github/theword/mcqq/commands/CommandExecutor.java
@@ -0,0 +1,50 @@
+package com.github.theword.mcqq.commands;
+
+import com.github.theword.mcqq.MCQQ;
+import com.github.theword.mcqq.commands.subCommands.HelpCommand;
+import com.github.theword.mcqq.commands.subCommands.ReloadCommand;
+import com.github.theword.mcqq.commands.subCommands.client.ReconnectAllCommand;
+import com.github.theword.mcqq.commands.subCommands.client.ReconnectCommand;
+import com.github.theword.mcqq.constant.BaseConstant;
+import com.mojang.brigadier.Command;
+import net.minecraft.commands.Commands;
+import net.minecraftforge.event.RegisterCommandsEvent;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.server.command.ConfigCommand;
+
+import static com.github.theword.mcqq.utils.Tool.handleCommandReturnMessage;
+
+@Mod.EventBusSubscriber(modid = MCQQ.MOD_ID)
+public class CommandExecutor {
+
+    @SubscribeEvent
+    public static void registerCommands(RegisterCommandsEvent event) {
+        event.getDispatcher().register(
+                Commands.literal(BaseConstant.COMMAND_HEADER)
+                        .requires(source -> source.hasPermission(2))
+                        .executes(context -> new HelpCommand().onCommand(context))
+                        .then(Commands.literal("help")
+                                .executes(context -> new HelpCommand().onCommand(context))
+                        )
+                        .then(Commands.literal("reload")
+                                .executes(context -> new ReloadCommand().onCommand(context))
+                        )
+                        .then(Commands.literal("client")
+                                .then(Commands.literal("reconnect")
+                                        .executes(context -> new ReconnectCommand().onCommand(context))
+                                        .then(Commands.literal("all")
+                                                .executes(context -> new ReconnectAllCommand().onCommand(context)))
+                                )
+                        ).then(Commands.literal("server")
+                                .executes(context -> {
+                                    // TODO Websocket Server Command
+                                    handleCommandReturnMessage.handleCommandReturnMessage(context, "Server command is not supported");
+                                    return Command.SINGLE_SUCCESS;
+                                })
+                        )
+        );
+        ConfigCommand.register(event.getDispatcher());
+    }
+
+}
